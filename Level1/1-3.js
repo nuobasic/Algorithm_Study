505762df024b87b1d305c405ebe5111d2638f4e4
nuobasic <nuobasic@naver.com>
2023-06-15 03:04:50 +0900

    소수 찾기 에라스토테네스체

--- Level1/1-3.js
+++ Level1/1-3.js
@@ -1,16 +1,34 @@
-function decimal(x) {
-  for (let i = 2; i < x; i++) {
-    if (x % i === 0) return false;
-  }
-  return true;
-}
+// function decimal(x) {
+//   for (let i = 2; i <= Math.sqrt(x) ; i++) {
+//     if (x % i === 0) return false;
+//   }
+//   return true;
+// }
+
+// function solution(n) {
+//   let answer = 0;
+
+//   for (let i = 2; i <= n; i++) {
+//     if (decimal(i)) answer++;
+//   }
+//   return answer;
+// }
+// console.log(solution(10));
 
 function solution(n) {
-  let answer = 0;
-
+  let arr = [];
   for (let i = 2; i <= n; i++) {
-    if (decimal(i)) answer++;
+    arr[i] = i;
   }
-  return answer;
+  for (let i = 2; i <= n; i++) {
+    for (let j = i + i; j <= n; j += i) {
+      if (arr[j] === 0) {
+        continue;
+      }
+      arr[j] = 0;
+    }
+  }
+  return arr.filter((e) => e !== 0).length;
 }
+
 console.log(solution(10));
