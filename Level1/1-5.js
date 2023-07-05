7d44ded511300166852de55d3f88878cac79cf8f
nuobasic <nuobasic@naver.com>
2023-07-06 02:27:33 +0900

    다트게임

---
+++ Level1/1-5.js
@@ -0,0 +1,23 @@
+function solution(dartResult) {
+  let num = 0;
+  let arr = [];
+
+  for (let i = 0; i < dartResult.length; i++) {
+    //console.log("111111111111111: " + dartResult.length);
+    if (!isNaN(dartResult[i])) {
+      num = Number(dartResult[i - 1]) === 1 ? 10 : Number(dartResult[i]);
+      // console.log(num);
+    }
+
+    if (dartResult[i] === "S") arr.push(num);
+    if (dartResult[i] === "D") arr.push(Math.pow(num, 2));
+    if (dartResult[i] === "T") arr.push(Math.pow(num, 3));
+    if (dartResult[i] === "*") {
+      arr[arr.length - 2] *= 2;
+      arr[arr.length - 1] *= 2;
+    }
+    if (dartResult[i] === "#") arr[arr.length - 1] *= -1;
+  }
+
+  return arr.reduce((a, b) => a + b, 0);
+}
