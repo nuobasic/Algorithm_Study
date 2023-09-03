4f61563410092a524632e59bbe799a30b2466603
nuobasic <nuobasic@naver.com>
2023-09-04 01:41:01 +0900

    기사단원의 무기

--- Level1/1-9.js
+++ Level1/1-9.js
@@ -1,15 +1,17 @@
 function solution(number, limit, power) {
-    const arr = Array.from(Array(number), (_, index) => index + 1);
-    return arr.map((num) => {
-        if (num === 1) return num;
-        let count = 0;
-        for (let n = 1; n <= Math.sqrt(num); n++) {
-            if (num % n === 0) {
-                count += (num / n === n) ? 1 : 2;
-            }
+  const arr = Array.from(Array(number), (_, index) => index + 1);
+  return arr
+    .map((num) => {
+      if (num === 1) return num;
+      let count = 0;
+      for (let n = 1; n <= Math.sqrt(num); n++) {
+        if (num % n === 0) {
+          count += num / n === n ? 1 : 2; //약수의 쌍 처리
         }
-        return count <= limit ? count : power;
-    }).reduce((a, b) => a + b, 0);
+      }
+      return count <= limit ? count : power;
+    })
+    .reduce((a, b) => a + b, 0);
 }
 
-console.log(solution(5,3,2,))
+console.log(solution(10, 3, 2));
